7c2bdfcf565653870700540be7e032dc175bae19
Corvushere <corvushere@gmail.com>
2023-01-16 23:26:35 +0300

    init2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+int x, y;
+Console.WriteLine("Введите число трёхзначное");
+y = Convert.ToInt32(Console.ReadLine());
+y = Math.Abs(y);
+if (y>99 && y<1000) {x = y / 10 % 10;
+Console.WriteLine("Вторая цифра справа: " + x); }
+else {Console.WriteLine("Вы ввели не трёхзначное число");}
